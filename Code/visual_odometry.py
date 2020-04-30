8b1c9f070cd5acafb1e8c1daeecd1488a9e27702
umang <umang.rastogi96@gmail.com>
2020-04-30 14:03:37 -0400

    [visual_odometry.py] Convert bayer image to color

--- Code/visual_odometry.py
+++ Code/visual_odometry.py
@@ -1,3 +1,4 @@
+import cv2
 from sys import argv
 from utils.data_prep import *
 
@@ -6,4 +7,10 @@ script, dataset_location = argv
 
 if __name__ == '__main__':
     filenames = extract_locations(str(dataset_location))
-
+    for file in filenames:
+        img_frame = cv2.imread(file, 0)
+        img_frame = cv2.cvtColor(img_frame, cv2.COLOR_BayerGR2BGR)
+        cv2.imshow("Image", img_frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break

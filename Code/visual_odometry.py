c87e0b81614186f97259dbf78ba4142d175551d3
umang <umang.rastogi96@gmail.com>
2020-05-01 11:01:15 -0400

    [visual_odometry.py] Save images instead of a video
    Need to access multiple frames which is not possible when reading from a video

--- Code/visual_odometry.py
+++ Code/visual_odometry.py
@@ -7,35 +7,35 @@ script, dataset_location, model_location = argv
 
 
 if __name__ == '__main__':
-    INPUT_VIDEO = 'undistorted_input.avi'
+    INPUT_IMAGES = 'undistorted_images/'
+    save_path = '../' + INPUT_IMAGES
     current_dir = os.path.abspath(os.getcwd())[:-4]
     cam_params = read_camera_model(str(model_location))
     filenames = extract_locations(str(dataset_location))
     # Create object to define video format and writer
     video_format = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
     # video_output = cv2.VideoWriter(str(output_location), video_format, 30.0, (1280, 960))
+    # Begin pre-processing pipeline
     # If input video does not exist, create it
-    if not os.path.exists(current_dir + INPUT_VIDEO):
-        print('Input video not found...Generating input video...')
-        video_input = cv2.VideoWriter('../' + INPUT_VIDEO, video_format, 30.0, (1280, 960))
+    if not os.path.exists(current_dir + INPUT_IMAGES):
+        print('Input images not found...Generating input images...')
+        os.makedirs(current_dir + INPUT_IMAGES[:-1])
+        count = 1399381444704913
         for file in filenames:
             img_frame = cv2.imread(file, 0)
             img_frame = cv2.cvtColor(img_frame, cv2.COLOR_BayerGR2BGR)
             undistorted_img = undistort_image(img_frame, cam_params[-1])
-            video_input.write(undistorted_img)
-        video_input.release()
+            cv2.imwrite(save_path + str(count) + '.png', undistorted_img)
+            count += 1
         print('Generated')
     else:
         print('Input video found!')
     print('Processing...')
-    video_capture = cv2.VideoCapture('../' + INPUT_VIDEO)
-    while True:
-        img_exists, img = video_capture.read()
-        if not img_exists:
-            break
-        cv2.imshow('Video frame', img)
+    images = extract_locations(save_path)
+    for image in images:
+        img = cv2.imread(image)
+        cv2.imshow('Frame', img)
         key = cv2.waitKey(1)
         if key == 27:
             break
-    # video_output.release()
-    cv2.destroyAllWindows()
+    print('Done!')

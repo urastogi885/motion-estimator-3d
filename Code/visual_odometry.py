d8455dfcbcd93ae28eb3d34c941aaf892ce609dd
umang <umang.rastogi96@gmail.com>
2020-05-01 11:51:58 -0400

    [visual_odometry.py] Add feature extraction using SIFT

--- Code/visual_odometry.py
+++ Code/visual_odometry.py
@@ -30,12 +30,37 @@ if __name__ == '__main__':
         print('Generated')
     else:
         print('Input video found!')
+    # Begin basic pipeline
     print('Processing...')
     images = extract_locations(save_path)
-    for image in images:
-        img = cv2.imread(image)
-        cv2.imshow('Frame', img)
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
+    for i in range(len(images) - 1):
+        # Read current and next frame
+        curr_img = cv2.imread(images[i], cv2.COLOR_BGR2GRAY)
+        next_img = cv2.imread(images[i+1], cv2.COLOR_BGR2GRAY)
+        # Crop current and next frame to retain only ROI
+        curr_img = curr_img[150:650, 0:1280]
+        next_img = next_img[150:650, 0:1280]
+        # Create SIFT detector object
+        sift = cv2.xfeatures2d.SIFT_create()
+        # Get key-points and descriptors for both frames
+        key_points_curr, descriptor_curr = sift.detectAndCompute(curr_img, None)
+        key_points_next, descriptor_next = sift.detectAndCompute(next_img, None)
+        # Define parameters for Flann-based matcher
+        index_params = dict(algorithm=0, trees=5)
+        search_params = dict(checks=50)
+        # Create object of Flann-based matcher
+        matcher = cv2.FlannBasedMatcher(index_params, search_params)
+        # Get matches between the current and next frame
+        matches = matcher.knnMatch(descriptor_curr, descriptor_next, k=2)
+        # Define empty list to store features of current and next frame
+        features_curr, features_next = [], []
+        # Employ ratio test
+        for _, (m, n) in enumerate(matches):
+            if m.distance < 0.5 * n.distance:
+                features_curr.append(key_points_curr[m.queryIdx].pt)
+                features_next.append(key_points_next[m.trainIdx].pt)
+        # cv2.imshow('Frame', curr_img)
+        # key = cv2.waitKey(1)
+        # if key == 27:
+        #     break
     print('Done!')

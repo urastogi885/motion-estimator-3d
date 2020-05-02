90d8b433b6d2763178991efe9bf4659545568f54
umang <umang.rastogi96@gmail.com>
2020-05-01 21:42:50 -0400

    [visual_odometry.py] Find final camera pose
    Plot the final pose

--- Code/visual_odometry.py
+++ Code/visual_odometry.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 from sys import argv
+from matplotlib.pyplot import scatter, show
 from utils.data_prep import *
 from utils.motion_estimator import MotionEstimator
 
@@ -37,6 +38,7 @@ if __name__ == '__main__':
     images = extract_locations(save_path)
     # Create object of motion estimator class
     motion_estimator = MotionEstimator((cam_params[0], cam_params[1]), (cam_params[2], cam_params[3]))
+    last_h_mat = motion_estimator.original_h
     for i in range(len(images) - 1):
         # Read current and next frame
         current_img = cv2.imread(images[i], cv2.COLOR_BGR2GRAY)
@@ -46,11 +48,24 @@ if __name__ == '__main__':
         next_img = next_img[150:650, 0:1280]
         # Extract key features using SIFT
         features_curr, features_next = motion_estimator.extract_features(current_img, next_img)
+        # Get the final fundamental matrix and inliers for current and next image frame
         fundamental_mat, inliers_curr, inliers_next = motion_estimator.ransac_with_8_point(features_curr, features_next)
+        # Evaluate the essential matrix from the fundamental matrix
         essential_mat = motion_estimator.calc_essential_matrix(fundamental_mat)
+        # Get all possible camera poses using the essential matrix
         cam_centers, rotation_mats = motion_estimator.estimate_camera_pose(essential_mat)
+        # Get the final camera pose
+        final_cam_center, final_r_mat = motion_estimator.disambiguate_camera_pose(cam_centers, rotation_mats,
+                                                                                  inliers_curr, inliers_next)
+        # Get the homogeneous transform using the final camera pose
+        last_h_mat = last_h_mat @ motion_estimator.get_homogeneous_matrix(final_r_mat, final_cam_center)
+        transform = last_h_mat @ motion_estimator.h_mat_last_row.T
+        print(transform[0], -transform[2])
+        scatter(transform[0], -transform[2], color='r')
+
         # cv2.imshow('Frame', curr_img)
         # key = cv2.waitKey(1)
         # if key == 27:
         #     break
+    show()
     print('Done!')
